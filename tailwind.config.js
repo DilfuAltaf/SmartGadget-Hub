600639b306bbd8e77b63603c7e3bcb9eca9d730f
DilfuAltaf <dilfualtaf07@gmail.com>
2025-01-23 14:26:35 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "header":"url('../img/background.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
